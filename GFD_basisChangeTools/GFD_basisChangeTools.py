95c2540d83e6188b275b470fc47e118546ee6e81
Alexandros Avdis <a.avdis@imperial.ac.uk>
2014-09-23 19:04:26 +0100

    Some extensions to basic module.

--- GFD_basisChangeTools/GFD_basisChangeTools.py
+++ GFD_basisChangeTools/GFD_basisChangeTools.py
@@ -1,4 +1,5 @@
-from math import sqrt, pi, sin, cos, atan2, acos
+from math import sqrt, pi, sin, asin, cos, tan, atan2, acos, degrees, radians
+import numpy as np
 
 def cartesian_2_sphericalPolar(positionVectorCartesian):
     '''Convert Cartesian coordinates to spherical-polar coordinates, in radians.
@@ -33,8 +34,8 @@ The origin of the Cartesian frame of reference is located at the centre of the s
     #Calculate azimuthal (phi), polar (theta) angles and distance from origin.
     [radius, theta, phi] = cartesian_2_sphericalPolar(positionVectorCartesian)
     #Calculate longitude and latitude
-    lon = phi*180.0/pi
-    lat = (pi/2 - theta)*180.0/pi
+    lon = degrees(phi)
+    lat = degrees(pi/2 - theta)
     
     positionVectorLonlat = [lon, lat, radius]
     return positionVectorLonlat
@@ -53,9 +54,93 @@ def lonlatradius_2_sphericalPolar(positionVectorLonLatRad):
     '''Convert longitude-latitude-radial coordinates on the surface of the Earth (in degrees) to Cartesian coordinates. Longitude and latitude must be in degrees, the azimuthal and polar angles are returned in radians.'''
     [longitude, latitude, radius] = positionVectorLonLatRad
     #Calculate azimuthal (phi), polar (theta) angles.
-    phi = longitude*pi/180.0
-    theta = pi/2 - latitude*pi/180.0
+    phi = np.radians(longitude)
+    theta = pi/2. - np.radians(latitude)
     return [radius, theta, phi]
+
+def cartesian_2_polarStereographic(positionVectorCartesian, surfaceRadius=6.37101e+06, southPoleCoordinates=None):
+    '''Convert Cartesian coordinates on a sphere to polar stereographic, where the stereographic plane is tangent to the north pole. The output coordinates are also normalised with the sphere diameter. If the southPoleCoordinates argument is specified the input coordinates basis is rotated.'''
+    x = np.double(positionVectorCartesian[0])
+    y = np.double(positionVectorCartesian[1])
+    z = np.double(positionVectorCartesian[2])
+    #Calculate output vertical coordinate
+    Z = (sqrt(x**2 + y**2 + z**2) - np.double(surfaceRadius))
+    #If the South pole coordinates are specified, rotate the inputi
+    # coordinate basis.
+    if southPoleCoordinates != None:
+        [x,y,z] = rotateCartesianBasis([x,y,z], southPoleCoordinates)
+    #Convert Cartesian coordinates into unit-sphere Cartesian Coordinates
+    x = x/(2.*np.double(surfaceRadius))
+    y = y/(2.*np.double(surfaceRadius))
+    z = z/(2.*np.double(surfaceRadius))
+    #Convert unit-diameter-sphere cordinates into polar stereographic, carefull with
+    # the South pole.
+    if z <= -np.double(0.5):
+        X = np.nan
+        Y = np.nan
+    else:
+        X = (x/(z + 0.5))
+        Y = (y/(z + 0.5))
+    return np.double([X,Y,Z])
+
+def rotateCartesianBasis(positionVectorCartesian, southPoleCoordinates):
+    '''Function rotating the planet-centered Cartesian basis of the input vector. The rotation angles are specified vie entering the coordinates of the point that will end on the south pole after the rotation.'''
+    southPoleLon = southPoleCoordinates[0]
+    southPoleLat = southPoleCoordinates[1]
+    angle1 = np.radians(-(90.+southPoleLon))
+    angle2 = np.radians((90.+southPoleLat))
+    #Rotate about the z-axis
+    rotationMatrix1 = np.array([[ np.cos(angle1), np.sin(angle1),  0.0],
+                                [-np.sin(angle1), np.cos(angle1),  0.0],
+                                [0.0,                  0.0,                  1.0]
+                               ])
+    #Rotation about the x-axis
+    rotationMatrix2 = np.array([[1.0,          0.0,                  0.0],
+                                [0.0,          np.cos(angle2),  np.sin(angle2)],
+                                [0.0,         -np.sin(angle2),  np.cos(angle2)]
+                               ])
+    rotationMatrix = np.dot(rotationMatrix2,rotationMatrix1)
+    rotatedCoords = np.dot(rotationMatrix, np.array(positionVectorCartesian))
+    return rotatedCoords.tolist()
+
+def lonlatradius_2_polarStereographic(positionVectorLonLatRad, surfaceRadius=6.37101e+06, southPoleCoordinates=None):
+    '''Convert longitude-latitude-radial coordinates on the surface of the Earth (in degrees) to polar stereographic, where the stereographic plane is tangent to the north pole. The output coordinates are also normalised with the sphere diameter.'''
+    [x,y,z] = lonlatradius_2_cartesian(positionVectorLonLatRad)
+    [X,Y,Z] = cartesian_2_polarStereographic([x,y,z], surfaceRadius, southPoleCoordinates)
+    return [X,Y,Z]
+
+def cartesian_2_unitDisk(positionVectorCartesian, surfaceRadius=6.37101e+06, southPoleCoordinates=None):
+    '''Convert Cartesian coordinates on a sphere to a plane unit disk, where the plane is tangent to the north pole.'''
+    #If the South pole coordinates are specified, rotate the input
+    # coordinate basis.
+    if southPoleCoordinates != None:
+        [x,y,z] = rotateCartesianBasis(positionVectorCartesian, southPoleCoordinates)
+    else:
+        x = positionVectorCartesian[0]
+        y = positionVectorCartesian[1]
+        z = positionVectorCartesian[2]
+    [lon,lat,rad] = cartesian_2_lonlatradius([x,y,z])
+    [X,Y,Z] = lonlatradius_2_unitDisk([lon,lat,rad], surfaceRadius)
+    return [X,Y,Z]
+
+def lonlatradius_2_unitDisk(positionVectorLonLatRad, surfaceRadius=6.37101e+06, southPoleCoordinates=None):
+    '''Convert longitude-latitude-radial coordinates on the surface of the Earth (in degrees) to a flat, unit disk. Similar to the North-polar Stereographic projection, but the "South" pole maps to points on the periphery of the unit circle.'''
+    #If the South pole coordinates are specified, rotate the input
+    # coordinate basis.
+    if southPoleCoordinates != None:
+        [x,y,z] = lonlatradius_2_cartesian(positionVectorLonLatRad)
+        [x,y,z] =  rotateCartesianBasis([x,y,z], southPoleCoordinates)
+        [longitude, latitude, radius] = cartesian_2_lonlatradius([x,y,z])
+    else:
+        longitude = positionVectorLonLatRad[0]
+        latitude = positionVectorLonLatRad[1]
+        radius = positionVectorLonLatRad[2]
+    alpha = longitude
+    r = (-1./180.)*latitude + 0.5
+    X = r*np.cos(np.radians(alpha))
+    Y = r*np.sin(np.radians(alpha))
+    Z = surfaceRadius - radius
+    return [X,Y,Z]
 
 def transform_tensor_sphericalPolar_2_cartesian(positionVectorSpherical, tensor):
     '''Function changing the basis of a tensor from zonal-meridional-radial basis to a Cartesian basis.
