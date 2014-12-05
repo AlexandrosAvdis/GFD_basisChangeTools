e9ca506bd654a92126abb98da88950c0b6e98469
Alexandros Avdis <a.avdis@imperial.ac.uk>
2014-12-05 18:27:02 +0000

    Adding a licence header to source files.

--- python/__init__.py
+++ python/__init__.py
@@ -1,2 +1,22 @@
+#    GFD_basisChangeTools. Basic python routines for change of coordinate
+#    reference systems, in a GFD context.
+#    Copyright (C) 2013, Alexandros Avdis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#    Author: Alexandros Avdis. a.avdis@imperial.ac.uk
+#    Code repository: https://github.com/AlexandrosAvdis/GFD_basisChangeTools
+
 '''
 '''
