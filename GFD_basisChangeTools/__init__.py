6329ff3c0a55d13f6e6d093bf928151ec4c42245
Alexandos Avdis <alexandros.avdis@gmail.com>
2017-05-02 13:20:49 +0100

    Correction in licence: GPL *v3*

--- GFD_basisChangeTools/__init__.py
+++ GFD_basisChangeTools/__init__.py
@@ -15,7 +15,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    Author: Alexandros Avdis. a.avdis@imperial.ac.uk
+#    Author: Alexandros Avdis. alexandros.avdis@gmail.com
 #    Code repository: https://github.com/AlexandrosAvdis/GFD_basisChangeTools
 
 '''
--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ from distutils.core import setup
 setup(name='GFD_basisChangeTools',
       version='1.0',
       description='Tensor basis change tools for geophysical fluid dynamics',
-      license='GPL',
+      license='GPLv3',
       packages=['GFD_basisChangeTools'],
      )
