b02353ecccfd703fefa6cbab27b9d6fda42474ce
Alexandros Avdis <a.avdis@imperial.ac.uk>
2018-07-19 22:16:21 +0100

    Setting up travis.

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@
 from distutils.core import setup
 
 setup(name='GFD_basisChangeTools',
-      version='1.0',
+      version='1.0.0',
+      author = "Alexandros Avdis",
+      provides=['GFD_basisChangeTools'],
       description='Tensor basis change tools for geophysical fluid dynamics',
       license='GPLv3',
       packages=['GFD_basisChangeTools'],
