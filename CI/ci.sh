c699eecd2991b6cafb9dcc27bb9344f5699e5d1e
Alexandros Avdis <a.avdis@imperial.ac.uk>
2018-07-20 13:38:01 +0100

    Moved the testing/deployment commands to a separate script, to allow more complex CI/D pipelines.

---
+++ CI/ci.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+if test $BRANCH_NAME = production
+then
+    # Install twine
+    pip install twine
+    # Create a source distribution
+    python setup.py sdist
+    # Upload to pypi
+    twine upload dist/*
+fi
